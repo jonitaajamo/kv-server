710948b4030bf74400469bbf85451d85784260d1
Joni Taajamo <joni.taajamo@outlook.com>
2023-05-01 03:02:11 +0300

    Implement basic tests

--- tests/test_kv_server.py
+++ tests/test_kv_server.py
@@ -1,8 +1,10 @@
 import os
+
 import pytest
 from fastapi.testclient import TestClient
 
-from main import app, read_environment_or_return_default
+from kv_server.key_value_store import KeyValueStore
+from kv_server.main import app, read_environment_or_return_default
 
 client = TestClient(app)
 
@@ -14,26 +16,27 @@ def test_read_environment_or_return_default_env_variable():
 
 def test_read_environment_or_return_default_no_env_variable():
     os.environ.pop("KV_DATA_FILE_PATH", None)
-    assert read_environment_or_return_default() == os.path.abspath(
-        os.path.join(os.path.dirname(__file__), "..", "data", "example.data")
-    )
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    expected_path = os.path.join(base_dir, "data", "example.data")
+    assert read_environment_or_return_default() == expected_path
 
 
 @pytest.fixture(scope="module")
 def mock_key_value_store(tmp_path_factory):
     data_file = tmp_path_factory.mktemp("data").joinpath("example.data")
     data_file.write_text("foo bar\nbaz qux\n")
-
     return KeyValueStore(str(data_file))
 
 
-def test_get_key_exists(mock_key_value_store):
+def test_get_key_exists(monkeypatch, mock_key_value_store):
+    monkeypatch.setattr("kv_server.main.key_value_store", mock_key_value_store)
     response = client.get("/key/foo")
     assert response.status_code == 200
-    assert response.json() == {"key": "bar"}
+    assert response.json() == {"foo": "bar"}
 
 
-def test_get_key_does_not_exist(mock_key_value_store):
+def test_get_key_does_not_exist(monkeypatch, mock_key_value_store):
+    monkeypatch.setattr("kv_server.main.key_value_store", mock_key_value_store)
     response = client.get("/key/nonexistent")
     assert response.status_code == 404
     assert response.json() == {"detail": "Key not found"}
--- tests/test_kv_store.py
+++ tests/test_kv_store.py
@@ -1,34 +1,42 @@
 import os
 import tempfile
+
+import pytest
+
 from kv_server.key_value_store import KeyValueStore
 
-def test_load_from_file():
+
+@pytest.fixture
+def key_value_store():
     with tempfile.NamedTemporaryFile(mode="w+", delete=False) as f:
         f.write("key1 value1\nkey2 value2\nkey3 value3\n")
         f.flush()
-        key_value_store = KeyValueStore(f.name)
-        assert len(key_value_store) == 3
-        assert key_value_store.get("key1") == "value1"
-        assert key_value_store.get("key2") == "value2"
-        assert key_value_store.get("key3") == "value3"
+        yield KeyValueStore(f.name)
 
-def test_verify_key():
-    key_value_store = KeyValueStore(os.path.abspath(os.path.join(os.path.dirname(__file__), "data", "example.data")))
+
+def test_load_from_file(key_value_store):
+    assert len(key_value_store) == 3
+    assert key_value_store.get("key1") == "value1"
+    assert key_value_store.get("key2") == "value2"
+    assert key_value_store.get("key3") == "value3"
+
+
+def test_verify_key(key_value_store):
     assert key_value_store.verify_key("key1")
     assert not key_value_store.verify_key("non-existent-key")
 
-def test_get():
-    key_value_store = KeyValueStore(os.path.abspath(os.path.join(os.path.dirname(__file__), "data", "example.data")))
+
+def test_get(key_value_store):
     assert key_value_store.get("key1") == "value1"
     assert key_value_store.get("non-existent-key") is None
 
-def test_set():
-    key_value_store = KeyValueStore(os.path.abspath(os.path.join(os.path.dirname(__file__), "data", "example.data")))
+
+def test_set(key_value_store):
     key_value_store.set("key4", "value4")
     assert len(key_value_store) == 4
     assert key_value_store.get("key4") == "value4"
 
-def test_flush():
-    key_value_store = KeyValueStore(os.path.abspath(os.path.join(os.path.dirname(__file__), "data", "example.data")))
+
+def test_flush(key_value_store):
     key_value_store.flush()
     assert len(key_value_store) == 0
